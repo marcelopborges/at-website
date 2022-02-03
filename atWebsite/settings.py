10301560ab6d8d9d58bb2c6c4d9834e0b1e3b1ce
Marcelo Borges <marcelopborges@outlook.com.br>
2022-02-03 08:31:05 -0300

    Criar primeira home Fixes #14

--- atWebsite/settings.py
+++ atWebsite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'atWebsite.base',
 ]
 
 MIDDLEWARE = [
@@ -116,7 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+# Configuração de ambiente de desenvolvimento
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'mediafiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
